4e1e0c4570068a5022c6d2a90750a6f73ed40b74
david.corrado <david.corrado@well.co>
2023-03-27 15:56:14 -0500

    Multiple content switch lose focus

--- app/src/main/java/com/example/composebugs/MainActivity.kt
+++ app/src/main/java/com/example/composebugs/MainActivity.kt
@@ -1,26 +1,42 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.composebugs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.composebugs.ui.theme.ComposeBugsTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBugsTheme {
-                Scaffold { paddingValues ->
-                    Column(modifier = Modifier.padding(paddingValues)) {
-                        Text("Android")
+                var pageShown by remember { mutableStateOf("1") }
+
+                LaunchedEffect(key1 = pageShown) {
+                    if (pageShown == "1") {
+                        delay(2500)
+                        pageShown = "2"
+                    } else if (pageShown == "2") {
+                        delay(2500)
+                        pageShown = "3"
+                    }
+                }
+                when (pageShown) {
+                    "1" -> {
+                        Text("Page 1")
+                    }
+                    "2" -> {
+                        Text("Page 2")
+                    }
+                    else -> {
+                        Text("Page 3")
                     }
                 }
             }
